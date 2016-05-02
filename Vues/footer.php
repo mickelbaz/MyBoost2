17329c75ec591efc879e5d2d6ee8e87511d7cc4c
estherlaurette <esther.laurette@gmail.com>
2016-05-02 11:34:14 +0200

    test2

--- Vues/footer.php
+++ Vues/footer.php
@@ -1,9 +1,9 @@
 <!--<link rel="stylesheet" type="text/css" href="footer.css">-->
 <footer>
     <div class="effet">
-        <a href="https://www.facebook.com/MyBoost-1136877939682445/?fref=ts"><img src="/MyBoost/Images/fb.png" alt="facebook"></a>
+        <a href="https://www.facebook.com/MyBoost-1136877939682445/?fref=ts"><img src="/MyBoost2/Images/fb.png" alt="facebook"></a>
         &nbsp;
-        <a href="https://twitter.com/myb00st"><img src="/MyBoost/Images/twitter.jpg" alt="twitter"></a>
+        <a href="https://twitter.com/myb00st"><img src="/MyBoost2/Images/twitter.jpg" alt="twitter"></a>
         &nbsp;
         <a href="#"><img src="/MyBoost2/Images/insta.jpg" alt="insta"></a>
         &nbsp;
