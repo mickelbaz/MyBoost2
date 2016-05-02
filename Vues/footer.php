a365e124db0e719463f656ae474cac4c137ff5cf
estherlaurette <esther.laurette@gmail.com>
2016-05-02 11:31:14 +0200

    test

--- Vues/footer.php
+++ Vues/footer.php
@@ -5,7 +5,7 @@
         &nbsp;
         <a href="https://twitter.com/myb00st"><img src="/MyBoost/Images/twitter.jpg" alt="twitter"></a>
         &nbsp;
-        <a href="#"><img src="/MyBoost/Images/insta.jpg" alt="insta"></a>
+        <a href="#"><img src="/MyBoost2/Images/insta.jpg" alt="insta"></a>
         &nbsp;
         MyBoost &copy; 2016 - Tous droits r&eacute;serv&eacute;s
     </div>
