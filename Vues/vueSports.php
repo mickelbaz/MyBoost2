4d6cfceab0fbd5b14d11f44a0d08d5b56c9bfaab
jblasalle <jeanbenoitlasalle@gmail.com>
2016-05-04 14:29:06 +0200

    modisportchoisi

--- Vues/vueSports.php
+++ Vues/vueSports.php
@@ -9,7 +9,7 @@
 
 	<div class="sports-aquatiques"><h2 class="categorie-sports">Sports aquatiques</h2>
 
-		<a href="sportChoisi.php?sport=<?php echo('Natation');?>"><img class="imgsport" src="../Images/clipart_Natation.png.jpg"></a>
+		<a href="../Vues/vueSportchoisi.php?sport=<?php echo('Natation');?>"><img class="imgsport" src="../Images/clipart_Natation.png.jpg"></a>
 		<a href="sportChoisi.php?sport=<?php echo('Wakeboard');?>"><img class="imgsport" src="../Images/clipart_Wakeboard.png.jpg"></a>
 
 	</div>
