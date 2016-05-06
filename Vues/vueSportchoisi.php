e7797f10fc90e5b9489abe0a83631fd907ee5b49
jblasalle <jeanbenoitlasalle@gmail.com>
2016-05-06 16:37:07 +0200

    chgmts théo/jb

--- Vues/vueSportchoisi.php
+++ Vues/vueSportchoisi.php
@@ -1,24 +1,55 @@
-<?php //require '../Vues/vueSportchoisi.php'; ?>
+<?php //require '../Vues/vueSportchoisi.php';
+
+try{
+
+$bdd=new PDO('mysql:host=localhost; dbname=myboostbdd; charset=utf8', 'root', 'root');
+}
+
+catch(Exception $e){
+  die('Erreur:'.$e->getMessage());
+}
+
+$description=mysql_query('SELECT description FROM sport WHERE nom="'.$_GET['sport'].'"');
+
+?>
+
+
   <html>
     <head>
       <body>
         <title>Sports</title>
 
-
+          <link rel="stylesheet" href="../Contenu/sports.css" >
           <link rel="stylesheet" href="../Contenu/sportchoisi.css">
     </head>
 <?php ?>
+<br>
+
     <fieldset class="sport">
       <legend>  <?php echo($_GET['sport']);?>
 
       </legend>
-        <?php echo($_GET{'description'});?>
+      <table width=100% border=0>
+        <td width="250px">
+          <img id="sport" src="../Images/clipart_<?php echo($_GET['sport']);?>.png">
+        </td>
+        <td>
+          <?php
+          while($donnees = mysql_fetch_array($description))
+          {
+            echo $donnees['description'];
+          }
+          ?>
+      </td>
+    </table>
+
     </fieldset>
+
       <br>
 
         <form action="">
           <input class="sport" type=submit value=Participer>
         </form>
-
+<br>
     </body>
   </html>
