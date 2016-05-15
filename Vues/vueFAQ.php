38e3d55c1d52efc133920ac31decfbe0a7ee1ae3
estherlaurette <esther.laurette@gmail.com>
2016-05-15 19:33:55 +0200

    FAQ

---
+++ Vues/vueFAQ.php
@@ -0,0 +1,38 @@
+<?php require "Vues/header.php"; ?>
+
+
+    <link type="text/css" rel="stylesheet" href="Contenu/FAQ.css"/>
+
+    <body>
+
+          	<h1>FAQ</h1>
+          	<h4>
+                Si vous ne trouvez pas de réponses adaptées, pensez à aller voir le forum. sinon nous somme a votre disposition sur serviceclientmyboost@gmail.com
+            </h4>
+
+          	<div class='questions'>
+				<dl>
+					<dt>Je n'arrive pas à me connecter sur le site?</dt><br />
+						<dd>Pour réussir à se connecter il faut tout d'abord avoir rempli le formulaire d'insciption de notre site.<br />
+							Au terme de l'inscription un e-mail vous a été envoyé pour valider votre inscription sur l'adresse mail que vous nous aurez fourni.<br />Pensez à cliquer sur le lien de confirmation présent dans ce mail pour valider votre inscription, vous ne pourrez pas vous inscrire sinon.<br /><br />
+						</dd>
+
+					<dt>J'aimerai changer ma photo de profil?</dt><br />
+						<dd>Rien de plus simple! <br />
+							Vous devez vous rendre sur votre profil après vous être connecté et cliquer sur "Mon compte" puis sur "modifier mon profil".<br />
+							Vous pourrez alors importer une nouvelle photo</dd></br>
+
+					<dt>Est-ce possible de changer son mot de passe?</dt><br />
+						<dd>Bien évidement il vous est possible de modifier votre mot de passe.<br />
+							Il suffit de vous rendre sur votre page profil après vous être connecté.<br />
+							Ensuite vous devez cliquer sur "Mon compte" puis sur "modifier mon profil".<br />
+							Ainsi vous pourrez effectuer votre changement de mot de passe.<br /><br />
+						</dd>
+
+					<dt>Comment rejoindre un groupe de sport?</dt><br />
+
+
+				</dl>
+			</div>
+		</body>
+  <?php require 'Vues/footer.php' ?>
