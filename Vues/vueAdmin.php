c847656a2b220f16c425e9f1b8fb3e66137fa2a5
adebarjac <alixdebarjac@MacBook-Air-de-Alix.local>
2016-05-17 10:26:26 +0200

    admin

---
+++ Vues/vueAdmin.php
@@ -0,0 +1,49 @@
+<?php require 'Vues/header.php'; ?>
+
+<link rel="stylesheet" type="text/css" href="Contenu/admin.css">
+
+<body>
+          	<h1> Bienvenue dans l'espace administrateur </h1>
+
+          	<div class=categorie>
+          		<table>
+            		<tr>
+                		<h4>
+                        <a href=".php" >Gérer les membres inscrits</a>
+                    </h4>
+
+            		</tr>
+        		</table>
+   	 		</div>
+
+   	 		<div class=categorie>
+          	<table>
+            		<tr>
+                    <h4>
+                        <a href=".php" >Gérer les groupes	</a>
+                    </h4>
+            		</tr>
+        		</table>
+   	 		</div>
+
+   	 		<div class=categorie>
+          		<table>
+            		<tr>
+                    <h4>
+                      <a href=".php" >Gérer les commentaires et messages du forum</a>
+                    </h4>
+            		</tr>
+        		</table>
+   	 		</div>
+
+   	 		<div class=categorie>
+          		<table>
+            		<tr>
+                    <h4>
+                        <a href=".php" >Gérer la rubrique FAQ</a>
+                    </h4>
+            		</tr>
+        		</table>
+   	 		</div>
+          </body>
+<?php require 'Vues/footer.php'; ?>
