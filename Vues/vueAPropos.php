cd0e825d9312f4a6e6b9a14c8cb96e468e1c9403
mickelbaz <mickelbaz@gmail.com>
2016-06-03 11:48:26 +0200

    amélioration à propos

--- Vues/vueAPropos.php
+++ Vues/vueAPropos.php
@@ -14,26 +14,41 @@
     border-width: thin;
     background-color: #edefed;">
 
+    <h1 style="text-align:center;">À propos de MyBoost</h1>
 
-    <h1 style="text-align:center;"> A quoi sert ce site ? </h1>
+    <div class="text" style="background-color: white;
+    border: solid;
+    border-color: #7d8287;
+    border-width: thin;
+    padding-top :2em;
+    padding-bottom: 2em;
+    padding-left: 1em;">
+
+
+    <h2 style="text-align:center;"> A quoi sert ce site ? </h3>
     <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac risus quis sapien vehicula varius sit amet id tellus. Nulla facilisi. Phasellus nec suscipit felis. Donec neque leo, convallis vel.
     </p>
 
-    <h1> Qui sommes nous ? </h1>
+    <h2 style="text-align:center;"> Qui sommes nous ? </h2>
     <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac risus quis sapien vehicula varius sit amet id tellus. Nulla facilisi. Phasellus nec suscipit felis. Donec neque leo, convallis vel.
     </p>
 
-    <p> <h1> Où sommes nous ? </h1>
+    <h2 style="text-align:center;"> Où sommes nous ? </h2>
+    <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac risus quis sapien vehicula varius sit amet id tellus. Nulla facilisi. Phasellus nec suscipit felis. Donec neque leo, convallis vel.
+    </p>
+
     <br></br>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.6676487334275!2d2.3260469153102514!3d48.84547780957049!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671ce3e69774f%3A0xab5ab8e13f32f28a!2s23+Rue+Notre+Dame+des+Champs%2C+75006+Paris!5e0!3m2!1sfr!2sfr!4v1462483813413" width="400" height="300" frameborder="0" style="border:0; text-align:center;" allowfullscreen></iframe>
-    </p>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.6676487334275!2d2.3260469153102514!3d48.84547780957049!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671ce3e69774f%3A0xab5ab8e13f32f28a!2s23+Rue+Notre+Dame+des+Champs%2C+75006+Paris!5e0!3m2!1sfr!2sfr!4v1462483813413" width="400" height="300" frameborder="0" style="border:0; margin-left:25em;" allowfullscreen></iframe>
 
-    <p> <h1> Comment nous contacter ? </h1>
+
+    <h2 style="text-align:center;"> Comment nous contacter ? </h2>
+    <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac risus quis sapien vehicula varius sit amet id tellus. Nulla facilisi. Phasellus nec suscipit felis. Donec neque leo, convallis vel.
     </p>
+    </div>
 
     </div>
   </body>
