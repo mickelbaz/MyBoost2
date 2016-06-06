5c6214454e2b15b5ad14c96376896687d815ba41
NatachaG <natacha.gerard@free.fr>
2016-06-06 16:47:25 +0200

    sport

--- Controleurs/controleurPropositionSport.php
+++ Controleurs/controleurPropositionSport.php
@@ -1,13 +1,20 @@
 <?php
 
-    //require '../Modeles/modelePropositionSport.php';
+
 
     function afficherproposport(){
 
       if(isset($_POST['envoyer']) && $_POST['envoyer']<>""){
         if($_POST["nomSport"]<>"" && $_POST['description'] <>"" && $_POST["type_de_sport"] <>""){
-          ajout_sport();
-          header('location: index.php?page=sports');
+          if(verif_sport($_POST['nomSport'])==true){?>
+              <script language="javascript">alert("Ce sport existe déjà !");</script>
+            <?php
+          }
+          else{
+            ajouter_sport();
+            header('location: index.php?page=sports');
+          }
+
         }
         else{?>
           <script language="javascript">alert("Un champ n'est pas rempli !");</script>
@@ -15,6 +22,5 @@
         }
       }
       require_once 'Vues/vuePropositionSport.php';
-
     }
 ?>
